b46fb5eb4b437fb03f8aafc181abc69d68d90e06
josephtule <54126483+josephtule@users.noreply.github.com>
2023-04-16 01:34:12 -0600

    add moon-sc state vector calculations

--- spacecraft.py
+++ spacecraft.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 from planets import earth, moon
 import scipy as sp
 
-global pi; pi = 3.14159
-global G; G = 6.6743e-20  # km^3/kg/s^2
-
+global pi
+pi = 3.14159
+global G
+G = 6.6743e-20  # km^3/kg/s^2
 
 
 def default_config():
@@ -33,21 +34,21 @@ class spacecraft:
     def __init__(self, config):
         self.config = default_config()
 
-
         for key in config.keys():
             self.config[key] = config[key]
-        
+
         self.dt = self.config['dt']
         if self.dt:
-            self.N = (self.config['tspan'][1] - self.config['tspan'][0])/self.dt + 1
+            self.N = (self.config['tspan'][1] -
+                      self.config['tspan'][0])/self.dt + 1
             self.N = int(self.N)
 
-   
-        self.state = np.zeros([self.N,len(self.config['state0'])])
-        self.m_state = np.zeros([self.N,len(self.config['m_state0'])])
-        self.state[0,:] = self.config['state0']
-        self.m_state[0,:] = self.config['m_state0']
-
+        self.state = np.zeros([self.N, len(self.config['state0'])])
+        self.m_state = np.zeros([self.N, len(self.config['m_state0'])])
+        self.state[0, :] = self.config['state0']
+        self.m_state[0, :] = self.config['m_state0']
+        self.scm = np.zeros([self.N, len(self.config['state0'])])
+        self.scm[0, :] = self.config['state0'] - self.config['m_state0']
 
         if self.config['coes']:
             pass
@@ -61,43 +62,45 @@ class spacecraft:
 
     def prop_moon(self):
         for i in range(self.N-1):
-            self.m_state[i+1,:] = self.rk4(self.m_eoms,self.m_state[i,:],i)
+            self.m_state[i+1, :] = self.rk4(self.m_eoms, self.m_state[i, :], i)
 
     def prop_sc(self):
         for i in range(self.N-1):
-            self.state[i+1,:] = self.rk4(self.sc_eoms,self.state[i,:],i)
+            self.state[i+1, :] = self.rk4(self.sc_eoms, self.state[i, :], i)
+            self.scm[i+1, :] = self.state[i+1, :] - self.m_state[i+1, :]
 
-    def sc_eoms(self,state,i):
+    def sc_eoms(self, state, i):
         r_EM = np.zeros([3])
-        r_EM = self.m_state[i,0:3]
+        r_EM = self.m_state[i, 0:3]
         # sc eom wrt to earth
-        r = np.zeros([3]); v = np.zeros([3])
-        r = state[0:3]; v = state[3:6]
+        r = np.zeros([3])
+        v = np.zeros([3])
+        r = state[0:3]
+        v = state[3:6]
         dxdt = np.zeros([6])
         dxdt[0:3] = v
-        
+
         egrav = - r * earth['mu'] / np.linalg.norm(r)**3
         mgrav = - 0*(r - r_EM) * moon['mu'] / np.linalg.norm(r - r_EM)**3
         dxdt[3:6] = egrav + mgrav
         return dxdt
 
-    def m_eoms(self,m_state,i):
-        r_EM = np.zeros([3]); v_EM = np.zeros([3])
+    def m_eoms(self, m_state, i):
+        r_EM = np.zeros([3])
+        v_EM = np.zeros([3])
         r_EM = m_state[0:3]
         v_EM = m_state[3:6]
         dxmdt = np.zeros([6])
         dxmdt[0:3] = v_EM
-        dxmdt[3:6] = - r_EM * earth['mu']/ np.linalg.norm(r_EM)**3
+        dxmdt[3:6] = - r_EM * earth['mu'] / np.linalg.norm(r_EM)**3
         return dxmdt
 
-    def rk4(self,f,x,i):
-        k1 = self.dt * f(x,i)
-        k2 = self.dt * f(x+k1/2,i)
-        k3 = self.dt * f(x+k2/2,i)
-        k4 = self.dt * f(x+k3,i)
+    def rk4(self, f, x, i):
+        k1 = self.dt * f(x, i)
+        k2 = self.dt * f(x+k1/2, i)
+        k3 = self.dt * f(x+k2/2, i)
+        k4 = self.dt * f(x+k3, i)
         return x + 1/6 * (k1 + 2*k2 + 2*k3 + k4)
-
-
 
     # def rk4(self,f, i):
     #     B = np.array([[0, 0, 0, 0, 0],
